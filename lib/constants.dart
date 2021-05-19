80f84b7b13e642769fb529619fcef58cb8cc07bf
alokhegde2 <hegdealok@yahoo.com>
2021-05-19 20:31:24 +0530

    Added some main constants

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+
+const kMainColor1 =Color(0xFF59ADFF);
+const kMainColor2 = Color(0xFF0065C7);
+const kPrimaryColor = Color(0xFF000918);
+const kTextColor = Color(0xFFFDFBFD);
+const kIconColor = Color(0xFFFDFBFD);
+const kSecondaryTextColor = Color(0xFF39475A);
+const kStatusColorWorking = Color(0xFFFFF30C);
+const kStatusColorError = Colors.red;
+const kStatusColorSuccess = Colors.green;
+
+const kDefaultPadding = 20.0;
