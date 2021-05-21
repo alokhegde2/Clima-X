5686cf6b3e75c908f148397c87987f7701904b2b
alokhegde2 <hegdealok@yahoo.com>
2021-05-21 21:15:15 +0530

    Handled location permission denied error

--- lib/controllers/location_controller.dart
+++ lib/controllers/location_controller.dart
@@ -1,5 +1,6 @@
 import 'package:climax/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,6 +9,7 @@ class LocationController extends GetxController {
   var latitude1 = ''.obs;
   var longitude1 = ''.obs;
   var isLocationEnabled = false.obs;
+  LocationPermission permission;
 
   @override
   void onInit() {
@@ -16,15 +18,32 @@ class LocationController extends GetxController {
   }
 
   Future<void> getCurrentLocation() async {
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        //Permissions are denied
+        SystemNavigator.pop();
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      SystemNavigator.pop();
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
     bool isLocationServiceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!isLocationServiceEnabled) {
       isLocationEnabled(false);
       print("Location off");
       Get.defaultDialog(
         title: "Failed to locate",
-        titleStyle: GoogleFonts.ubuntu(color: kTextColor,height: 2.0),
+        titleStyle: GoogleFonts.ubuntu(color: kTextColor, height: 2.0),
         content: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
           child: Text(
             "Failed to locate,please enable network-based locaton service in Settings.",
             style: GoogleFonts.ubuntu(color: kTextColor),
@@ -49,7 +68,6 @@ class LocationController extends GetxController {
         ],
         backgroundColor: kPrimaryColor,
         radius: 10.0,
-
       );
       return;
     }
